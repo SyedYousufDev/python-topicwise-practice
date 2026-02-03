7dc044c0445bb3952fc127ae79a3b0ea3132e2d3
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-03 20:39:20 +0500

    Added a prgram file: Make a dictionary to take and and show names and and their favorite programming languages

---
+++ chapter_05_dictionary_sets/05_dict_for_friends.py
@@ -0,0 +1,20 @@
+dict={}
+
+name=input("Enter a friend name:")
+lang=input("Enter it favorite language:")
+dict.update({name:lang})
+
+name=input("Enter a friend name:")
+lang=input("Enter it favorite language:")
+dict.update({name:lang})
+
+name=input("Enter a friend name:")
+lang=input("Enter it favorite language:")
+dict.update({name:lang})
+
+name=input("Enter a friend name:")
+lang=input("Enter it favorite language:")
+dict.update({name:lang})
+
+
+print(dict)
