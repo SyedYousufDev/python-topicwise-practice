61000890b52924ced0f81b0e0e57d58a0cfe968a
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-04 21:39:23 +0500

    added a file to chapter 9 conditional expressions name:greatest_of_four_numbers

---
+++ chapter_06_conditional_expressions/01_greatest_of_four_numbers.py
@@ -0,0 +1,14 @@
+
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+c=int(input("Enter a number: "))
+d=int(input("Enter a number: "))
+
+if a > b and a > c and a > d:
+    print("This number is greater ->", a)
+elif b > a and b > c and b > d:
+    print("This number is greater ->", b)
+elif c > a and c > b and c > d:
+    print("This number is greater ->", c)
+elif d > a and d > b and d > c:
+    print("This number is greater ->", d)
