ac63c7c8936eb4777eb6450cc7fdf1c5079d14b8
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-05 10:49:39 +0500

    Added a 4th program file for anlyzing string whether it have 10 characters or not

---
+++ chapter_06_conditional_expressions/04_username_10chars.py
@@ -0,0 +1,9 @@
+# Write a program to find whether a given username contains less than 10 
+# characters or not. 
+
+username=str(input("Enter Your Username Thank You."))
+
+if(len(username)<10):
+  print("The Username You have entered have less than 10 Characters.Thank You")
+else: 
+  print("The Username You have entered donot have less than 10 Characters.Thank You") 
