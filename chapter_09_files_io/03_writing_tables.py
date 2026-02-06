a7428c3d1e2c06ed0ffae6fcb64c83fa1f7b4a7e
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-06 11:40:53 +0500

    Added 3rd program file which prints and make files for tables 1 to 20 in a folder

---
+++ chapter_09_files_io/03_writing_tables.py
@@ -0,0 +1,14 @@
+
+
+def generate_table(n):
+    table=""
+    for i in range(1,11):
+        table += f"{n} * {i} = {n*i}\n"
+    with open(f"tables/table_{n}.txt","w") as f:
+        f.write(table)
+    
+
+
+
+for k in range(2,21):
+    generate_table(k)
