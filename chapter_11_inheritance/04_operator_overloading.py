2c6d6c4aba3d5ba6717866625ab5df967f1a75bc
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-11 15:01:48 +0500

    Modified the 4th porgram file by adding feature of multiplication

--- chapter_11_inheritance/04_operator_overloading.py
+++ chapter_11_inheritance/04_operator_overloading.py
@@ -7,6 +7,10 @@ class complex:
     def __add__(self,c2):
         return complex(self.r+c2.r,self.i+c2.i)
     
+    def __mul__(self, c2):
+        return complex(self.r * c2.r - self.i * c2.i, self.r * c2.i + self.i * c2.r)
+
+    
     def __str__(self):
         return f"{self.r} + {self.i}i"
     
@@ -16,3 +20,4 @@ c1=complex(1,3)
 c2=complex(2,5)
 
 print(c1 + c2)
+print(c1*c2)
