ce7d84b221e3c0d5605f393986c48ba00ceaa456
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-14 22:35:40 +0500

    Added a comment to 5th program file

--- chapter_11_inheritance/05_len_dunder_function.py
+++ chapter_11_inheritance/05_len_dunder_function.py
@@ -1,3 +1,8 @@
+#Details about the dunder
+
+#In Python, __len__ is a special (dunder) method that defines what should be returned when the built-in len() function is called on an object. When you write len(obj), Python internally calls obj.__len__(). This method must return a non-negative integer representing the size or length of the object. By defining __len__ in a custom class, you can make your objects behave like built-in containers such as lists or strings.
+
+
 class Vector:
     def __init__(self, x, y, z):
         self.x = x
