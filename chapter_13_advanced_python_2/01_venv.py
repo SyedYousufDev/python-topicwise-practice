b697bd58edbb06bcbfc5d2aa66d43fed1c2dacf9
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-02-18 13:08:06 +0500

    Added 1st program file where all steps are given to create virtual environment

---
+++ chapter_13_advanced_python_2/01_venv.py
@@ -0,0 +1,12 @@
+#Step 1 creating
+# "virtualenv env1" ->creates a a virtual environment
+
+#step 2 activating
+# ".\env1\Scripts\activate.ps1"
+
+
+# Step 3 installing some pakages
+ # we can validate it by printing the requirments using commnad "pip freeze > requirements.txt"
+
+#step 4 Deactivate after working 
+ # "deactivate"
